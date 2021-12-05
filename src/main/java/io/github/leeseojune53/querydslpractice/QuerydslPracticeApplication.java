b7ec1333af32bfb47914b2ac01f20f1c10e3cbc2
leeseojune <sung07288346@gmail.com>
2021-12-05 11:17:01 +0900

    init

---
+++ src/main/java/io/github/leeseojune53/querydslpractice/QuerydslPracticeApplication.java
@@ -0,0 +1,13 @@
+package io.github.leeseojune53.querydslpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/leeseojune53/querydslpractice/QuerydslPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leeseojune53.querydslpractice;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+class QuerydslPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
