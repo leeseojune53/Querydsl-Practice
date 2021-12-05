e11c76cf6183b16bcd48b1e01748504aa0ebc12d
leeseojune <sung07288346@gmail.com>
2021-12-05 14:18:00 +0900

    ⚡ :: 불필요한 변수 제거

--- src/main/java/io/github/leeseojune53/querydslpractice/domain/post/service/QueryPostService.java
+++ src/main/java/io/github/leeseojune53/querydslpractice/domain/post/service/QueryPostService.java
@@ -1,6 +1,5 @@
 package io.github.leeseojune53.querydslpractice.domain.post.service;
 
-import io.github.leeseojune53.querydslpractice.domain.comment.domain.repositories.CommentRepository;
 import io.github.leeseojune53.querydslpractice.domain.post.domain.Post;
 import io.github.leeseojune53.querydslpractice.domain.post.domain.repositories.PostRepository;
 import io.github.leeseojune53.querydslpractice.domain.post.presentation.dto.response.QueryPostResponse;
@@ -13,10 +12,9 @@ import org.springframework.stereotype.Service;
 public class QueryPostService {
 
 	private final PostRepository postRepository;
-	private final CommentRepository commentRepository;
 
 	public QueryPostResponse execute(Long id) {
-		Post post = postRepository.getById(id);
+		Post post = postRepository.getPostById(id);
 		return new QueryPostResponse(post.getContent(),
 				postRepository.getCommentById(id));
 	}
