e67136fba8c850e9ad48426f8a8f8a5b2cb8fe7c
leeseojune <sung07288346@gmail.com>
2021-12-05 11:17:30 +0900

    📑 :: JpaAuditingConfig

---
+++ src/main/java/io/github/leeseojune53/querydslpractice/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package io.github.leeseojune53.querydslpractice.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
